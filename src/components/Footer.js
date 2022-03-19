e7aad8f21f4888cd0f743bc2ac433a6b2c0cb82d
febryan <febryan19pemangkat@gmail.com>
2022-03-19 22:58:52 +0700

    ok

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <footer>
+            <h2>Copyright by Febryan</h2>
+            <p>Created using React</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import Hello from "./Hello"
+
+const Main = () => {
+    
+    // Mengirim nama dan pekerjaan
+    return(
+        <main>
+            <Hello nama="Adit" pekerjaan="Designer"/>
+            <Hello nama="Febryan" pekerjaan="Expert Marketing"/>
+            <Hello nama="Kalalo" pekerjaan="Staff Bank"/>
+            <Hello nama="Ben" pekerjaan="Manager PT.LEX"/>
+            <Hello nama="Ucok" pekerjaan="Montir"/>
+        </main>
+    )
+}
+
+export default Main
